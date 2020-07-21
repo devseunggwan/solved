d7fdae363ad76f24d890f2acd3abe272e5e4f247
devSeungGwan <yanhis97531@gmail.com>
2020-07-21 09:59:54 +0900

    ADD: Beakjoon Code(Solved.ac S2)

--- code/11055.py
+++ code/11055.py
@@ -1,11 +1,13 @@
 N = int(input())
 A = list(map(int, input().strip().split()))
 dp = [0 for i in range(N)]
+ans = 0
 
 for i in range(N):
-    dp[i] = 1
+    dp[i] = A[i]
     for j in range(i):
         if(A[i] > A[j]): 
-            dp[i] = max(dp[i], dp[j]+1)
+            dp[i] = max(dp[i], dp[j]+A[i])
+    ans = max(ans, dp[i])
 
-print(max(dp))
+print(ans)
