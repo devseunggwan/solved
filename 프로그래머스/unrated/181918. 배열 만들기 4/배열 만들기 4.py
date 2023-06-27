8aab05ba316211e5e2e2f3ea9309dab40b96f38c
송승관 <yanhis97531@gmail.com>
2023-06-27 16:54:39 +0900

    [unrated] Title: 배열 만들기 4, Time: 9.31 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    stk = []
+    i = 0
+    
+    while i != len(arr):
+        if (stk == []) or (stk != [] and stk[-1] < arr[i]):
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] >= arr[i]:
+            stk.pop()
+    
+    return stk
