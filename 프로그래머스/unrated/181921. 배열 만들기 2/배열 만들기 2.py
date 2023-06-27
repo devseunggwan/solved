def66f473d2dd68d06a3dac3ec4e8385c56ab010
송승관 <yanhis97531@gmail.com>
2023-06-27 14:20:45 +0900

    [unrated] Title: 배열 만들기 2, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,15 @@
+from itertools import permutations
+
+def solution(l, r):
+    arr = ["5"]
+    cur = 0
+    
+    for i in range(7):
+        for j in range(cur, cur + (2 ** i)):
+            arr.extend([arr[j] + "0", arr[j] + "5"])
+    
+        cur += 2 ** i
+
+    arr = [num for num in map(int, arr) if l <= num <=r]
+    
+    return [-1] if not arr else arr
