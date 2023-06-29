cb706fa45d69f54e30fc41a6a9fa7dfd648eebd0
송승관 <yanhis97531@gmail.com>
2023-06-29 23:10:13 +0900

    [unrated] Title: 커피 심부름, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,16 @@
+def solution(order):
+    menu_board = {
+        "iceamericano": 4500, 
+        "americanoice": 4500,
+        "hotamericano": 4500, 
+        "americanohot": 4500,
+        "icecafelatte": 5000, 
+        "cafelatteice": 5000,
+        "hotcafelatte": 5000, 
+        "cafelattehot": 5000,
+        "americano": 4500,
+        "cafelatte": 5000,
+        "anything": 4500,
+    }
+    
+    return sum([menu_board[menu] for menu in order])
