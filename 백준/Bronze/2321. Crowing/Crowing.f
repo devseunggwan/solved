223a5504339b5707287faba3364be6232f30a18a
Seunggwan Song <yanhis97531@gmail.com>
2023-07-26 22:29:42 +0900

    [Bronze III] Title: Crowing, Time: 0 ms, Memory: 1408 KB -BaekjoonHub

---
+++ 백준/Bronze/2321. Crowing/Crowing.f
@@ -0,0 +1,8 @@
+program crowing
+    PRINT "(a)", ",~"
+    PRINT "(a)", "( 9> //)"
+    PRINT "(a)", " )(_///"
+    PRINT "(a)", "( \_>/"
+    PRINT "(a)", " \__/"
+    PRINT "(a)", "  ^^"
+end program crowing
