e1e8005e855c96740f198208187419479149db67
Seunggwan Song <yanhis97531@gmail.com>
2024-07-30 12:19:43 +0900

    [Gold V] Title: 용액, Time: 160 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,21 @@
+N = int(input())
+M = list(map(int, input().strip().split()))
+
+start = 0
+end = N - 1
+value = 2_000_000_000
+answer = (1_000_000_000, 1_000_000_000)
+
+while start != end:
+    if abs(M[start] + M[end]) <= abs(sum(answer)):
+        answer = (M[start], M[end])
+
+    if M[start] + M[end] > 0:
+        end -= 1
+    elif M[start] + M[end] < 0:
+        start += 1
+    else:
+        break
+
+
+print(" ".join(map(str, answer)))
